fd822ec0d1dce5bf308fd8193d1a41bf86a99f87
RakDiary <diarihenikajarakotoarisoa9@gmail.com>
2022-05-09 18:04:20 +0200

    project structure

--- app/src/main/java/com/example/smartkids/view/MainActivity.kt
+++ app/src/main/java/com/example/smartkids/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.smartkids
+package com.example.smartkids.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.smartkids.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
