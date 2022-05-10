1a14b8b4634be911f227f28991a3a2fbceb15f9f
Rajaobelina <rajaobelina@yahoo.com>
2022-05-10 12:25:40 +0300

    Splash Screen

---
+++ app/src/main/java/com/smartkid/dd/activity/SplashActivity.java
@@ -0,0 +1,54 @@
+package com.smartkid.dd.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.smartkid.dd.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        this.loadAnimation();
+
+        Thread splashScreenStarter = new Thread() {
+            public void run() {
+                try {
+                    int delay = 0;
+                    while (delay < 2000) {
+                        sleep(150);
+                        delay = delay + 100;
+                    }
+                    startActivity(new Intent(SplashActivity.this, AuthActivity.class));
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    finish();
+                }
+            }
+
+        };
+        splashScreenStarter.start();
+    }
+
+    private void loadAnimation(){
+        // Animation tv_title TextView
+        img = (ImageView) findViewById(R.id.imageView);
+        Animation animImg = AnimationUtils.loadAnimation(this, R.anim.bounce);
+        animImg.setStartOffset(500);
+        img.startAnimation(animImg);
+    }
+}
