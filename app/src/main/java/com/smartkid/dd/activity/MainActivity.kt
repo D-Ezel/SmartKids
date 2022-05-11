2c19971437943717487433ad5fa731befbf01e83
RakDiary <diarihenikajarakotoarisoa9@gmail.com>
2022-05-11 14:07:22 +0200

    bottom navigation design

--- app/src/main/java/com/smartkid/dd/activity/MainActivity.kt
+++ app/src/main/java/com/smartkid/dd/activity/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.smartkid.dd.activity
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.smartkid.dd.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.smartkid.dd.R
+import com.smartkid.dd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,13 +21,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-        navView.menu.findItem(R.id.navigation_dashboard).title = "Categories"
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_settings))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
---
+++ app/src/main/java/com/smartkid/dd/activity/ui/settings/SettingsFragment.kt
@@ -0,0 +1,52 @@
+package com.smartkid.dd.activity.ui.settings
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.smartkid.dd.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SettingsFragment()
+    }
+
+    private var _binding: FragmentSettingsBinding? = null
+
+    private val binding get() = _binding!!
+
+    private lateinit var viewModel: SettingsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val settingsViewModel =
+            ViewModelProvider(this).get(SettingsViewModel::class.java)
+
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textSettings
+        settingsViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    /*override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(SettingsViewModel::class.java)
+        // TODO: Use the ViewModel
+    }*/
+
+}
