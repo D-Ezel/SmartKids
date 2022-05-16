d88f61650a769e494c9ebf3b7774839e37839009
RakDiary <diarihenikajarakotoarisoa9@gmail.com>
2022-05-16 17:39:32 +0200

    notifications with adapter

--- app/src/main/java/com/smartkid/dd/activity/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/smartkid/dd/activity/ui/notifications/NotificationsFragment.kt
@@ -7,9 +7,21 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.smartkid.dd.R
+import com.smartkid.dd.activity.ui.home.adapter.PopularAdapter
+import com.smartkid.dd.activity.ui.home.adapter.RecentViewAdapter
+import com.smartkid.dd.activity.ui.home.helper.PopularHelper
+import com.smartkid.dd.activity.ui.notifications.adapter.NotificationsAdapter
+import com.smartkid.dd.activity.ui.notifications.helper.NotificationsHelper
 import com.smartkid.dd.databinding.FragmentNotificationsBinding
+import java.sql.Time
 
-class NotificationsFragment : Fragment() {
+class NotificationsFragment : Fragment(), NotificationsAdapter.ListItemClickListener {
+
+    var notificationsRecycler: RecyclerView? = null
+    var notificationsAdapter: RecyclerView.Adapter<*>? = null
 
     private var _binding: FragmentNotificationsBinding? = null
 
@@ -33,11 +45,36 @@ class NotificationsFragment : Fragment() {
             textView.text = it
         }
 
+        val notificationsViewRecyclerCaller: RecyclerView = binding.notificationsRecycler
+        notificationsRecycler = notificationsViewRecyclerCaller.findViewById(R.id.notifications_recycler)
+        notificationViewRecycler()
+
         return root
+    }
+
+    private fun notificationViewRecycler() {
+
+        notificationsRecycler?.setHasFixedSize(true)
+        notificationsRecycler?.setLayoutManager(
+            LinearLayoutManager(
+                activity,
+                LinearLayoutManager.VERTICAL,
+                false
+            )
+        )
+        val notificationCardList: ArrayList<NotificationsHelper> = ArrayList()
+        notificationCardList.add(NotificationsHelper(R.drawable.ic_dashboard_black_24dp, "Guess Instrument","Il y a 1 min", Time(1224548)))
+        notificationCardList.add(NotificationsHelper(R.drawable.ic_dashboard_black_24dp, "Sounds Animals", "Il y a 3 jours", Time(1224548)))
+        notificationCardList.add(NotificationsHelper(R.drawable.ic_dashboard_black_24dp, "Answer the response", "Il y a 5 min", Time(1224548)))
+        notificationCardList.add(NotificationsHelper(R.drawable.ic_dashboard_black_24dp, "New Game", "Il y a 5 min", Time(1224548)))
+        notificationsAdapter = NotificationsAdapter(notificationCardList, this)
+        notificationsRecycler?.setAdapter(notificationsAdapter)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    override fun onNotificationsListClick(clickedItemIndex: Int) {}
 }
