20d0b8e024346325d997611ec52fb2cd5ecc37f5
RakDiary <diarihenikajarakotoarisoa9@gmail.com>
2022-05-21 20:46:31 +0200

    setup different fragment of games (instrument, animaux, pays, lettre nombres)

--- app/src/main/java/com/smartkid/dd/activity/ui/main/tabs/EducationGamesFragment.kt
+++ app/src/main/java/com/smartkid/dd/activity/ui/main/tabs/EducationGamesFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.smartkid.dd.R
+import com.smartkid.dd.activity.ui.category.IDENTIFICATION
 
 class EducationGamesFragment : Fragment() {
 
@@ -14,6 +15,20 @@ class EducationGamesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        val id = this.activity?.intent?.getStringExtra(IDENTIFICATION)
+        if(id.equals("6284eededaac815be2cbe1dd")) {
+            //instrument
+            return inflater.inflate(R.layout.fragment_instrument, container, false)
+        } else if(id.equals("6284efbddaac815be2cbe1de")){
+            //animaux
+            return inflater.inflate(R.layout.fragment_animal_game, container, false)
+        } else if(id.equals("6284efcfdaac815be2cbe1df")){
+            //number-letter
+            return inflater.inflate(R.layout.fragment_letter_number, container, false)
+        } else if(id.equals("6284efdfdaac815be2cbe1e0")) {
+            //Pays
+            return inflater.inflate(R.layout.fragment_pays, container, false)
+        }
         return inflater.inflate(R.layout.fragment_education_games, container, false)
     }
 
